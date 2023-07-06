60bc9e250014ac526b7338fb096894cf0f6cdd47
helixmw <wonganichipofya98@gmail.com>
2023-07-06 13:30:28 +0200

    *

--- routes/Employees.js
+++ routes/Employees.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Employee = require('../models/Employee.js');
-const {check, validationResult} =require('express-validator');
+const {check} =require('express-validator');
+
 const formValidation = [
     check('firstname').notEmpty().withMessage('Firstname is required'),
     check('lastname').notEmpty().withMessage('Lastname is required.'),
@@ -10,97 +11,13 @@ const formValidation = [
     check('phone').notEmpty().withMessage('Phone Address is required.')
 ];
 
-router.get('/', async(req, res) => {
-    try{
-        try{
-            const emps = await Employee.find();
-            res.status(200).json({ success: emps});
-        }catch(e){
-        res.status(404).json({error: "No employees available at the moment." + e.message});   
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try later"});
-    }
-});
+//EmployeesController
+const { getAllEmployees, addNewEmployee, getEmployee, editEmployee, deleteEmployee } = require('../controllers/EmployeesController')
 
-
-router.post('/', formValidation , async(req,res) => {
-    try{
-        try{
-            const result = validationResult(req);
-            if(!result.isEmpty()){
-                res.status(401).json({error: result.mapped()});
-            }else{
-
-                const emp = await Employee.create({
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    email: req.body.email,
-                    address: req.body.address,
-                    phone: req.body.phone
-                });
-                let rs = await emp.save();
-                res.status(201).json({success: rs});
-            }
-        }catch(e){
-            res.status(401).json({error: "Invalid Entry." + e.message});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try later"});
-    }
-});
-
-router.put('/:id', formValidation , async(req,res) => {
-    try{
-        try{
-            let result = validationResult(req);
-            if(!result.isEmpty()){
-                res.status(401).json({error: result.mapped()});
-            }else{
-                let emp = await Employee.findById(req.params.id);
-                emp.firstname = req.body.firstname;
-                emp.lastname = req.body.lastname;
-                emp.email = req.body.email;
-                emp.address = req.body.address;
-                emp.phone = req.body.phone;
-                let rs = await emp.save();
-                res.status(200).json({success: rs});
-            }
-        }catch(e){
-            res.status(401).json({error: "Invalid Entry." + e.message});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try later"});
-    }
-});
-
-
-router.delete('/delete/:id', async(req,res) => {
-    try{
-        try{
-            await Employee.findByIdAndDelete(req.params.id);
-            res.status(200).json({success: true});
-        }catch(e){
-            res.status(404).json({error: "Employee not found."});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try later"});
-    }
-});
-
-router.get('/:id', async(req,res) => {
-    try{
-        try{
-            let rs = await Employee.findById(req.params.id);
-            res.status(200).json({success: rs});
-        }catch(e){
-            res.status(404).json({error: "Employee not found."});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try later"});
-    }
-});
-
-
+router.get('/', getAllEmployees);
+router.post('/', formValidation , addNewEmployee);
+router.put('/:id', formValidation , editEmployee);
+router.delete('/delete/:id', deleteEmployee);
+router.get('/:id', getEmployee);
 
 module.exports = router;
--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User.js');
+//const User = require('../models/User.js');
 const {check, validationResult} =require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+const { editProfile, getUsers, signUp, login, logout, userProfile, assignRole, specificUser, deleteUser } = require('../controllers/AuthController');
 
 const formValidation = [
     check('username').notEmpty().withMessage('Username is required'),
@@ -43,182 +45,33 @@ function verifyToken(req,res,next){
 }
 }
 
-router.get('/', verifyToken , async(req,res) => {
-    try{
-        try{
-            let users = await User.find();
-            if(users.length == 0){
-                res.status(404).json({error: "No users available."});
-            }else{
-                res.status(200).json({success: users});
-            }
-        }catch(e){
-            res.status(403).json({error:"Something went wrong. Please try later"});
-        }
-    }catch(e){
-        res.status(500).json({error:"Something went wrong. Please try later"});
-    }
-});
+router.get('/', verifyToken , getUsers);
 
 //signup
-router.post('/signup', formValidation , async(req,res)=>{
-    try{
-        try{
-            let errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(403).json({errors: errors.mapped()});
-            }else{
-                const salt = await bcrypt.genSalt();
-                let password = await bcrypt.hash(req.body.password, salt);
-            let user = new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: password,
-            });
-            let result = await user.save();
-            let token = createToken(result._id);
-            res.status(201).cookie('jwt', token, {httpOnly: true}).json({user: result});
-        }
-        }catch(e){
-            res.status(403).json({error: e.message});  
-        }
-    }catch(e){
-        res.status(500).json({error: e.message}); 
-    }
-});
+router.post('/signup', formValidation , signUp);
 
 //Login
-router.post('/login', loginValidation , async(req,res) => {
-    try{
-        try{
-            let errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(400).json({error: errors.mapped()});
-            }else{
-                let user = await User.findOne({ email: req.body.email });
-                let auth = await bcrypt.compare(req.body.password, user.password);
-                if(auth){
-                    let token = createToken(user._id);
-                    res.status(200).cookie('jwt', token, {httpOnly: true}).json({success: user});
-                }else{
-                    res.status(403).json({error: "Unauthorized"});         
-                }
-            }
-        }catch(e){
-            res.status(500).json({error: e.message});
-        }
-    }catch(e){
-        res.status(500).json({error: e.message});
-    }
-
-});
+router.post('/login', loginValidation , login);
 
 //Logout
-router.get('/logout', (req,res) => {
-    try{
-        res.cookie('jwt','')
-        .status(200)
-        .json({success: "Logged out"});
-    }catch(e){
-        res.status(500).json({error: e.message});
-    }  
-});
+router.get('/logout', logout);
 
 //User Profile
-router.get('/profile', verifyToken , async(req,res)=>{
-    try{
-        try{
-        let user = await User.findById(req.userId);
-        if(user == null){
-            res.status(404).json({error: "Sorry, cannot find user."})
-        }else{
-            res.status(200).json(user);
-        }
-        }catch(e){
-    res.status(500).json({error: "Something went wrong. Please try again later"});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try again later"});
-    }
-});
+router.get('/profile', verifyToken , userProfile);
 
 
 //Edit User Profile
-router.get('/profile/edit', verifyToken , async(req,res)=>{
-    try{
-        try{
-        let user = await User.findById(req.userId);
-        if(user == null){
-            res.status(404).json({error: "Sorry, cannot find user."})
-        }else{
-            const salt = await bcrypt.genSalt();
-            let password = await bcrypt.hash(req.body.password, salt);
-            user.username = req.body.username;
-            user.email = req.body.email;
-            user.password = password;
-            let rs = await user.save();
-            res.status(200).json(rs);
-        }
-        }catch(e){
-    res.status(500).json({error: "Something went wrong. Please try again later"});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try again later"});
-    }
-});
+router.get('/profile/edit', verifyToken , editProfile);
 
 //Assign Role
-router.get('/user/role/:id', verifyToken , async(req,res)=>{
-    try{
-        try{
-        let user = await User.findById(req.params.id);
-        if(user == null){
-            res.status(404).json({error: "Sorry, cannot find user."})
-        }else{
-            user.role = req.body.role;
-            let rs = await user.save();
-            res.status(200).json(rs);
-        }
-        }catch(e){
-    res.status(500).json({error: "Something went wrong. Please try again later"});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try again later"});
-    }
-});
+router.get('/user/role/:id', verifyToken , assignRole);
 
 
 //Find specific user
-router.get('/profile/:id', verifyToken , async(req,res)=>{
-    try{
-        try{
-        let user = await User.findById(req.params.id);
-        if(user == null){
-            res.status(404).json({error: "Sorry, cannot find user."})
-        }else{
-            res.status(200).json(user);
-        }
-        }catch(e){
-    res.status(500).json({error: "Something went wrong. Please try again later"});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try again later"});
-    }
-});
+router.get('/profile/:id', verifyToken , specificUser);
 
 //Delete or deactivate
-router.delete('/delete', verifyToken, async(req,res) => {
-    try{
-        try{
-            await User.findByIdAndDelete(req.userId);
-            res.cookie('jwt','').status(200).json({success: "User deleted"});
-        }catch(e){
-            res.status(500).json({error: "Something went wrong. Please try again later"});
-        }
-    }catch(e){
-        res.status(500).json({error: "Something went wrong. Please try again later"});
-    }
-});
+router.delete('/delete', verifyToken, deleteUser);
 
 
 
